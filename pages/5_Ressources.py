b18f6971a98b8262f1cc5e622eed0bb5de8cc66b
Blueberry012 <156487524+Blueberry012@users.noreply.github.com>
2024-03-30 23:11:28 +0100

    Update 5_Ressources.py

--- pages/5_Ressources.py
+++ pages/5_Ressources.py
@@ -2,8 +2,8 @@ import pandas as pd
 import  matplotlib.pyplot as plt
 import streamlit as st
 
-df1 = pd.read_csv('data\pdl_1.csv')
-df2 = pd.read_csv('data\pdl_2.csv')
+df1 = pd.read_csv('data/pdl_1.csv')
+df2 = pd.read_csv('data/pdl_2.csv')
 
 st.title("Bases de données")
 
